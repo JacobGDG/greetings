12caf08643d4e56ba32384c9b188202f6a91a875
JacobGDG <jacob.greenwood@gmail.com>
2021-02-20 13:10:13 +0000

    Add hello project using base package
    `cd` into `hello/` to run bas module.

---
+++ hello/go.mod
@@ -0,0 +1,5 @@
+module hello
+
+go 1.16
+
+require github.com/JacobGDG/greetings v0.1.0
---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "fmt"
+  "github.com/JacobGDG/greetings"
+)
+
+func main() {
+    fmt.Println(greetings.Hello("World"))
+}
